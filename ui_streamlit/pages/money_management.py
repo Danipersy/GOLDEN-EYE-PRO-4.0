308d27dab6c1491dc3e09821d510314a52cf4e84
Danipersy <persicodaniele@gmail.com>
2026-02-28 22:23:10 +0100

    Update money_management.py

--- ui_streamlit/pages/money_management.py
+++ ui_streamlit/pages/money_management.py
@@ -1,4 +1,3 @@
-# ui_streamlit/pages/money_management.py
 import streamlit as st
 from strategy.money_manager import render_money_manager_panel
 
@@ -6,10 +5,12 @@ def render():
     """TAB 4: Money Management"""
     if 'detail_data' in st.session_state:
         data = st.session_state.detail_data
+        # Usa score dal segnale se disponibile, altrimenti default 65
+        signal_score = data.get('score', 65)
         render_money_manager_panel(
             data.get('p', 0),
             data.get('atr', 0),
-            65
+            signal_score
         )
     else:
         st.info("ℹ️ Carica prima un dettaglio asset")
